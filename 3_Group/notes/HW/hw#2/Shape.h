d4c86d840ba81654caf8b12ab0b38e3d2c840f06
Lafy7798 <89852564+Lafy7798@users.noreply.github.com>
2023-11-19 14:46:10 +0900

    Add files via upload

---
+++ 3_Group/notes/HW/hw#2/Shape.h
@@ -0,0 +1,10 @@
+class Shape {
+	Shape* next;
+protected:
+	virtual void draw() = 0;
+public:
+	Shape() { next = NULL; }
+	void paint();
+	Shape* getNext() { return next; }
+	Shape* add(Shape* p) { next = p; return p; }
+}; 
